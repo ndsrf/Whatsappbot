4ba60e29b89a68cc05c76e3730986ff4d5232224
Javier García Magna <jgarciamagna@gmail.com>
2024-04-26 17:00:45 +0000

    Added constants

---
+++ constants.js
@@ -0,0 +1,4 @@
+module.exports = Object.freeze({
+    N8N_SERVER : 'homeassistant.lan:5678',
+    BOT_NAME: 'ForoGPT'
+});
--- main.js
+++ main.js
@@ -4,8 +4,10 @@ const qrcode = require('qrcode-terminal');
 const express = require('express');
 const app = express();
 
+var constants = require('./constants');
+
 app.listen(3000, () => {
- console.log("Server running on port 3000");
+ console.log("Server running on port 3000, with n8n running in " + constants.N8N_SERVER + " and bot name " + constants.BOT_NAME);
 });
 
 
@@ -44,8 +46,8 @@ const client = new Client({
             }
     
             if (message.body === '!ayuda') {
-                    client.sendMessage(message.from, 'Hola! soy el asistente del foro!');
+                    client.sendMessage(message.from, 'Hola! soy ' + constants.BOT_NAME + ', el asistente del foro!');
             }
     });
 
-//client.initialize();
+client.initialize();
